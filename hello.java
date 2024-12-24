e7d839047d0990a3ddb1a564d0432521f8bf104a
mohan7401647399 <mtm.kcs@gmail.com>
2024-12-24 08:41:32 +0530

    first java

---
+++ hello.java
@@ -0,0 +1,21 @@
+// This is a basic Java program
+// class hello{
+
+//     // The main method is the entry point of any Java program
+//     public static void main(String args[]) {
+//         // Print a message to the console
+//         System.out.print("Hello, World!");
+//     }
+// }
+
+
+
+// // This is a basic Java program
+public class hello{
+
+    // The main method is the entry point of any Java program
+    public static void main(String[] args) {
+        // Print a message to the console
+        System.out.println("Hello, World!");
+    }
+}

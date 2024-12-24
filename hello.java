6c482bf3b5bc570cc024deb3f321f04ed4e45945
mohan7401647399 <mtm.kcs@gmail.com>
2024-12-24 22:16:50 +0530

    Java variables

--- hello.java
+++ hello.java
@@ -11,11 +11,19 @@
 
 
 // // This is a basic Java program
-public class hello{
+// public class hello{
 
-    // The main method is the entry point of any Java program
-    public static void main(String[] args) {
-        // Print a message to the console
-        System.out.println("Hello, World!");
-    }
-}
+//     // The main method is the entry point of any Java program
+//     public static void main(String[] args) {
+//         // Print a message to the console
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+
+//              Variables
+// int num = 5;
+// float floatNum = 5.99f;
+// char letter = 'D';
+// boolean isTrue = true;
+// String site = "www.google.com";

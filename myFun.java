9c109f9e25ba5da6dd5405bf652e41a8279d1ffd
mohan7401647399 <mtm.kcs@gmail.com>
2024-12-25 21:24:32 +0530

    Java - variables

---
+++ myFun.java
@@ -0,0 +1,38 @@
+// This is a basic Java program
+// class hello{
+
+//     // The main method is the entry point of any Java program
+//     public static void main(String args[]) {
+//         // Print a message to the console
+//         System.out.print("Hello, World!");
+//     }
+// }
+
+
+
+// // This is a basic Java program
+// public class hello{
+
+//     // The main method is the entry point of any Java program
+//     public static void main(String[] args) {
+//         // Print a message to the console
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+
+//              Variables
+// public class myFun{
+//     public static void main(String[] args){
+//         int myNum = 5;                                   // integer (whole number)
+//         float floatNum = 5.99f;                          // floating point number
+//         char letter = 'D';                               // character
+//         boolean isTrue = true;                          //  boolean
+//         String site = "www.google.com";                 //  string
+//         System.out.print(myNum);
+//         System.out.print(floatNum);
+//         System.out.print(letter);
+//         System.out.print(isTrue);
+//         System.out.print(site);
+//     }
+// }

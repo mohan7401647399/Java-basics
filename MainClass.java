3ed21843231a6edc57ab8d166292646812c75df2
mohan7401647399 <mtm.kcs@gmail.com>
2025-06-10 07:06:04 +0530

    Java - Thread

--- MainClass.java
+++ MainClass.java
@@ -1278,3 +1278,125 @@
 // 		}
 // 	}
 // }
+
+
+
+//		10 - Jun - 2025
+//	Thread
+
+// package java_practice;
+
+// class A extends Thread{
+// 	public void run() {
+// 		for(int i=0; i<5; i++) {
+// 			System.out.println("A class....");
+// 		}
+// 	}
+// }
+
+// class B extends Thread{
+// 	public void run() {
+// 		for(int i=0; i<5; i++) {
+// 			System.out.println("B class....");
+// 		}
+// 	}
+// }
+
+// public class MainClass {
+	
+// 	public static void main(String[] args) {
+// 		A a1 = new A();
+// 		B b1 = new B();
+		
+// 		a1.start();
+// 		b1.start();
+// 	}
+// }
+
+
+//  sleep()
+
+// package java_practice;
+
+// class A extends Thread{
+// 	public void run() {
+// 		for(int i=0; i<5; i++) {
+// 			System.out.println("A class....");
+// 			try {
+// 				Thread.sleep(5);
+// 			} catch (InterruptedException e) {
+// 				e.printStackTrace();
+// 			}
+// 		}
+// 	}
+// }
+
+// class B extends Thread{
+// 	public void run() {
+// 		for(int i=0; i<5; i++) {
+// 			System.out.println("B class....");
+// 			try {
+// 				Thread.sleep(5);
+// 			} catch (InterruptedException e) {
+// 				e.printStackTrace();
+// 			}
+// 		}
+// 	}
+// }
+
+// public class MainClass {
+	
+// 	public static void main(String[] args) {
+// 		A a1 = new A();
+// 		B b1 = new B();
+		
+// 		a1.start();
+// 		b1.start();
+// 	}
+// }
+
+
+
+//		10 - Jun - 2025
+//	setPriority
+
+// package java_practice;
+
+// class A extends Thread{
+// 	public void run() {
+// 		for(int i=0; i<5; i++) {
+// 			System.out.println("A class....");
+// 			try {
+// 				Thread.sleep(5);
+// 			} catch (InterruptedException e) {
+// 				e.printStackTrace();
+// 			}
+// 		}
+// 	}
+// }
+
+// class B extends Thread{
+// 	public void run() {
+// 		for(int i=0; i<5; i++) {
+// 			System.out.println("B class....");
+// 			try {
+// 				Thread.sleep(5);
+// 			} catch (InterruptedException e) {
+// 				e.printStackTrace();
+// 			}
+// 		}
+// 	}
+// }
+
+// public class MainClass {
+	
+// 	public static void main(String[] args) {
+// 		A a1 = new A();
+// 		B b1 = new B();
+		
+// 		b1.setPriority(5);
+// 		a1.setPriority(5);		
+// 		a1.start();
+// 		b1.start();
+// 	}
+// }

9d68de7f14c903e84d617e59d5edc54b91921601
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2025-10-30 22:33:52 +0530

    Springboot - security JWT

--- java-Course/Springboot/Security/JWT_CRUD_security_30102025/src/main/java/com/example/demo/JwtCrudSecurity30102025Application.java
+++ java-Course/Springboot/Security/JWT_CRUD_security_30102025/src/main/java/com/example/demo/JwtCrudSecurity30102025Application.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicSecurity21102025Application {
+public class JwtCrudSecurity30102025Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BasicSecurity21102025Application.class, args);
+		SpringApplication.run(JwtCrudSecurity30102025Application.class, args);
 	}
+
 }
--- java-Course/Springboot/Security/basic_security_21102025/src/main/java/com/example/demo/security/SecurityConfig.java
+++ java-Course/Springboot/Security/basic_security_21102025/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
 
 @Configuration
 @EnableWebSecurity
@@ -12,13 +13,25 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable()) // disable CSRF for testing with Postman
-				.authorizeHttpRequests(
-						auth -> auth
+//		http.csrf(csrf -> csrf.disable()) // disable CSRF for testing with Postman
+//				.authorizeHttpRequests(
+//						auth -> auth
+//						.requestMatchers("/api/employees/**").authenticated()
+//						.anyRequest().permitAll())
+//				.httpBasic();	//	Enable basic authentication
+//
+//		return http.build();
+		
+		
+        // URL or Endpoint level
+		return http
+				.csrf(csrf -> csrf.disable())
+				.httpBasic(Customizer.withDefaults())	// postman (or) Rest full service authentication
+				.formLogin(form -> form.disable())	// MVC application authentication
+				.authorizeHttpRequests(requests -> requests	//	Allows restricting access 
 						.requestMatchers("/api/employees/**").authenticated()
-						.anyRequest().permitAll())
-				.httpBasic();	//	Enable basic authentication
-
-		return http.build();
+						.anyRequest().permitAll()
+					)
+				.build();
 	}
 }
